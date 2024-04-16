b99c08b9369298f85ee59d0308fe214146031cf1
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-04-16 15:24:36 +0630

    findMany with prisma orm

--- script.ts
+++ script.ts
@@ -2,13 +2,18 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const createdUser = await prisma.user.createMany({
+    const users = await prisma.user.findMany({})
+    console.log(users)
+    /**
+     const createdUser = await prisma.user.createMany({
         data: [
             {name: 'john'},
             {name: 'cherry'}
         ]
     })
     console.log(createdUser)
+     */
+    
 }
 
 main()
